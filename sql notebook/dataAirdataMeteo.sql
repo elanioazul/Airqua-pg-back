6c171ddd6f830d7c3ef2e25f28643aed1ea4e474
hugo <hugo.herrador@gmail.com>
2020-06-17 22:36:11 +0200

    Change query for not showing nulls

--- sql notebook/dataAirdataMeteo.sql
+++ sql notebook/dataAirdataMeteo.sql
@@ -117,3 +117,7 @@ ADD CONSTRAINT estacion_id FOREIGN KEY (estacion) REFERENCES meteostations (codi
 
 
 select h01, h02, h03, h02, h03, h04, h05, h06, h07, h08, h09, h10, h11, h12, h13, h14, h15, h16, h17, h18, h19, h20, h21, h22, h23, h24 from meteodata where (estacion = 39 AND magnitud = 86 AND ano = 2019 and dia = 4 and mes = 12);
+
+UPDATE meteostations_2
+SET temperatura = ''
+WHERE temperatura IS NULL;
--- src/controllers/stationsAir.controllers.js
+++ src/controllers/stationsAir.controllers.js
@@ -47,7 +47,7 @@ export async function getAirStationbyId(req, res) {
         const id = req.params.id;
         const response = await pool.query(
             'SELECT codigo_cor, estacion, direccion, lon_geogra, lat_geogra, altitud, no2, so2, co, pm10, pm2_5, o3, btx, hc FROM airstations WHERE codigo_cor = $1',
-            [id]
+            [id] 
         )
         res.json(response.rows)
         console.log(req.params.codigo_cor)
